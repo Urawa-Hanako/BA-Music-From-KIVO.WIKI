ab620f18cc6058fbf4990a5619841722205fb234
Urawa-Hanako <cc596804860@outlook.com>
2025-01-12 19:02:27 +0800

    Update kivo.py

--- kivo.py
+++ kivo.py
@@ -1,12 +1,15 @@
 import requests
 import re
 
+save_path = 'E:/音乐/BlueArchive' # 保存路径(注意\需要转义，文件路径中也可以使用/或\\)
+ids = range(1, 602)  # 要下载的歌曲 id 范围
+
 def clean_filename(filename):
     # 使用正则表达式替换 windows 文件名中的无效字符
     return re.sub(r'[<>:"/\\|?*]', '_', filename)
 
-for i in range(451, 602): # 目前有450首歌曲
-    url = 'https://api.kivo.wiki/api/v1/musics/' + str(i)
+for id in ids:
+    url = 'https://api.kivo.wiki/api/v1/musics/' + str(id)
 
     response = requests.get(url, timeout=10)  # 设置请求超时
     response.encoding = 'utf-8'  # 设置编码
@@ -18,30 +21,10 @@ for i in range(451, 602): # 目前有450首歌曲
         title = clean_filename(title)  # 清理文件名
         download_url = 'https:' + api_data['data']['file']
     except:
-        print(f'歌曲{i}下载失败')
-        continue  # 跳过下载失败的歌曲（结束本轮i循环）
+        print(f'歌曲{id}下载失败')
+        continue
 
-    print(f'开始下载{title}')  # 添加日志输出
-    with open('E:/音乐/BlueArchive/' + title + '.ogg', 'wb') as f: # 注意\需要转义，文件路径中也可以使用/或\\
-        f.write(requests.get(download_url).content)  # 写入歌曲文件
+    print(f'开始下载{title}')
+    with open(save_path + '/' + title + '.ogg', 'wb') as f:
+        f.write(requests.get(download_url).content)
 print('下载完成')
-
-
-# 用BeautifulSoup解析网页内容:
-
-# from bs4 import BeautifulSoup
-
-# # 解析网页内容
-# soup = BeautifulSoup(response.text, 'html.parser')
-
-# print(soup.prettify())  # 打印网页内容
-
-# # 假设歌曲信息在某个特定的标签里
-# songs = soup.find_all('div', class_='song-item')  # 替换为实际的标签和类名
-
-# for song in songs:
-#     title = song.find('h2').text  # 替换为实际的标签
-#     link = song.find('a')['href']  # 获取链接
-#     print(f'歌曲名: {title}, 链接: {link}')
-
-# # 注意遵循网站的爬虫规则，检查robots.txt文件
